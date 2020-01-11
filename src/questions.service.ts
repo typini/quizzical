c9dd1b80e10cfca574f03f5989946deb5e9640ad
typini <typini@gmail.com>
2020-01-11 08:37:51 -0700

    Deploy ---

--- src/questions.service.ts
+++ src/questions.service.ts
@@ -10,7 +10,7 @@ export class QuestionsService {
   constructor(private http: HttpClient) {}
 
   public getQuizzes() {
-    return this.http.get(`../assets/quiz-list.json`).pipe(
+    return this.http.get(`./assets/quiz-list.json`).pipe(
       map((result: any[]) => {
         return result.map(r => new Quiz(r.label, r.name, r.description, r.fileName));
       })
@@ -18,7 +18,7 @@ export class QuestionsService {
   }
 
   public getQuestions(fileName: string) {
-    return this.http.get(`../assets/${fileName}.json`).pipe(
+    return this.http.get(`./assets/${fileName}.json`).pipe(
       map((result: any[]) => {
         return result.map(r => new Question(r.label, r.choices));
       })
